c682d7186fa3b0a28922a4c72c9072d890fc4fa9
Kevin Sana <kevin.sana@outlook.com>
2022-05-10 00:45:02 -0500

    added dotenv to hide apikey and connected mongoose to mongodb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
+
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
+
+mongoose.connect(process.env.API_KEY);
 
 app.listen(PORT, () => {
     console.log(`Server running on : ${PORT}`);
